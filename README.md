0a4a1a33e38d51ee7e7bbe3d4a049231a9a81380
31core <im31core@yandex.com>
2022-01-24 14:42:22 +0000

    Update 'ast_tree_build' function.

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ Anicat是一个基于C语言的编程语言, 支持动态解释、执行, 并且
 ### 0x2 命令行选项
 ```shell
 $ make #编译
+$ make test #测试主程序
 $ make clean #清理输出文件
 ```
--- src/ast.c
+++ src/ast.c
@@ -56,19 +56,28 @@ void ast_node_append(AST_NODE *ast, AST_NODE *target, int p)
 	ast->nodes[p] = target;
 	ast->nodes[p + 1] = 0;
 }
+
+struct layer
+{
+	AST_NODE *node;
+	int potition;
+};
+
 /* 组建AST树 */
 void ast_tree_build(AST_NODE *ast, TOKEN tk[])
 {
 	AST_NODE *node_layer[100];
+	struct layer layers[100];
+	int p = -1;
 	int node_layer_position = 0;
-	int i = 0;
-	while(tk[i].type != TOKEN_TYPE_UNKOWN)
+	int token_index = 0;
+	while(tk[token_index].type != TOKEN_TYPE_UNKOWN)
 	{
 		/* 函数定义 */
-		if(tk[i].type == TOKEN_TYPE_KEYWORD)
+		if(tk[token_index].type == TOKEN_TYPE_KEYWORD)
 		{
 			/* 函数定义 */
-			if(!strcmp(tk[i].name, "func"))
+			if(!strcmp(tk[token_index].name, "func"))
 			{
 				ast->type = AST_TYPE_FUNC_DEF;
 
@@ -80,11 +89,11 @@ void ast_tree_build(AST_NODE *ast, TOKEN tk[])
 				ast->nodes[1]->type = AST_TYPE_PARAMS;
 				ast->nodes[2]->type = AST_TYPE_CODE_BLOCK;
 
-				strcpy(ast->nodes[0]->data, tk[i + 1].name);
-				i += 1;
+				strcpy(ast->nodes[0]->data, tk[token_index + 1].name);
+				token_index += 1;
 			}
 			/* 变量声明 */
-			else if(!strcmp(tk[i].name, "var"))
+			else if(!strcmp(tk[token_index].name, "var"))
 			{
 				ast->type = AST_TYPE_VAR_DECLEAR;
 				ast_node_append(ast, ast_node_manage_alloc(), 0); //NAME
@@ -93,35 +102,57 @@ void ast_tree_build(AST_NODE *ast, TOKEN tk[])
 				ast->nodes[0]->type = AST_TYPE_NAME;
 				ast->nodes[1]->type = AST_TYPE_VAR_TYPE;
 
-				strcpy(ast->nodes[0]->data, tk[i + 1].name);
-				strcpy(ast->nodes[1]->data, tk[i + 3].name);
+				strcpy(ast->nodes[0]->data, tk[token_index + 1].name);
+				strcpy(ast->nodes[1]->data, tk[token_index + 3].name);
 			}
 		}
-		else if(tk[i].type == TOKEN_TYPE_LS_BKT)
+		else if(tk[token_index].type == TOKEN_TYPE_SPLIT)
 		{
+			ast_node_append(layers[p].node, ast_node_manage_alloc(), layers[p].potition);
+			ast = layers[p].node->nodes[layers[p].potition];
+			layers[p].potition += 1;
+		}
+		/* ( */
+		else if(tk[token_index].type == TOKEN_TYPE_LS_BKT)
+		{
+			p += 1;
 			node_layer[node_layer_position] = ast;
 			node_layer_position += 1;
 			if(ast->type == AST_TYPE_FUNC_DEF)
 			{
-				ast = ast->nodes[1];
+				/* ast->nodes[1] is params block */
+				layers[p].node = ast->nodes[1];
+				layers[p].potition = 0;
+				ast_node_append(ast->nodes[1], ast_node_manage_alloc(), 0);
+				ast = ast->nodes[1]->nodes[0];
+				layers[p].potition += 1;
 			}
 		}
-		else if(tk[i].type == TOKEN_TYPE_LL_BKT)
+		/* { */
+		else if(tk[token_index].type == TOKEN_TYPE_LL_BKT)
 		{
+			p += 1;
 			node_layer[node_layer_position] = ast;
 			node_layer_position += 1;
 			if(ast->type == AST_TYPE_FUNC_DEF)
 			{
-				ast = ast->nodes[2];
+				/* ast->nodes[1] is code block */
+				layers[p].node = ast->nodes[2];
+				layers[p].potition = 0;
+				ast_node_append(ast->nodes[2], ast_node_manage_alloc(), 0);
+				ast = ast->nodes[2]->nodes[0];
+				layers[p].potition += 1;
 			}
 		}
-		else if(tk[i].type == TOKEN_TYPE_RS_BKT ||
-		tk[i].type == TOKEN_TYPE_RM_BKT ||
-		tk[i].type == TOKEN_TYPE_RL_BKT)
+		/* right brackets */
+		else if(tk[token_index].type == TOKEN_TYPE_RS_BKT ||
+		tk[token_index].type == TOKEN_TYPE_RM_BKT ||
+		tk[token_index].type == TOKEN_TYPE_RL_BKT)
 		{
+			p += 1;
 			node_layer_position -= 1;
 			ast = node_layer[node_layer_position];
 		}
-		i += 1;
+		token_index += 1;
 	}
 }
